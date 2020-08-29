7d7bc0f03aa254a06b1a4c04456fb7cb951bc1c0
geekchow <geekchow@live.com>
2020-08-29 22:56:07 +0800

    use GlobalConfiguration instead of Descriptor.

--- src/main/java/io/jenkins/plugins/securepostscript/SecurePostScript.java
+++ src/main/java/io/jenkins/plugins/securepostscript/SecurePostScript.java
@@ -4,30 +4,22 @@ import hudson.EnvVars;
 import hudson.Extension;
 import hudson.model.*;
 import hudson.model.listeners.RunListener;
-import jenkins.model.Jenkins;
-import net.sf.json.JSONArray;
-import net.sf.json.JSONObject;
 import org.jenkinsci.plugins.scriptsecurity.sandbox.groovy.SecureGroovyScript;
-import org.jenkinsci.plugins.scriptsecurity.scripts.ClasspathEntry;
-import org.jenkinsci.plugins.scriptsecurity.scripts.ApprovalContext;
-import org.kohsuke.stapler.StaplerRequest;
-
-import java.net.MalformedURLException;
-import java.util.List;
-import java.util.ArrayList;
 
 @Extension
-public class SecurePostScript extends RunListener<Run<?, ?>> implements Describable<SecurePostScript> {
+public class SecurePostScript extends RunListener<Run<?, ?>> {
 
   @Override
   public void onCompleted(final Run run, final TaskListener listener) {
     final EnvVars envVars = getEnvVars(run, listener);
-
-    if (run.getResult().isWorseThan(getDescriptorImpl().getResultCondition())) {
+    SecurePostScriptConfiguration cfg = SecurePostScriptConfiguration.get();
+    System.out.println("result: " + cfg.getRunCondition());
+    if (run.getResult().isWorseThan(SecurePostScriptConfiguration.get().getResultCondition())) {
       return;
     }
 
-    final SecureGroovyScript script = getDescriptorImpl().getSecureGroovyScript();
+    final SecureGroovyScript script = SecurePostScriptConfiguration.get().getSecureGroovyScript();
+    System.out.println("script to be executed: " + script.getScript());
     try {
       new GroovyScriptRunner().run(script, envVars, listener);
     } catch (final Throwable e) {
@@ -43,65 +35,6 @@ public class SecurePostScript extends RunListener<Run<?, ?>> implements Describa
     } catch (final Throwable e) {
       e.printStackTrace();
       return null;
-    }
-  }
-
-  public Descriptor<SecurePostScript> getDescriptor() {
-    return getDescriptorImpl();
-  }
-
-  public DescriptorImpl getDescriptorImpl() {
-    return (DescriptorImpl) Jenkins.get().getDescriptorOrDie(SecurePostScript.class);
-  }
-
-  @Extension
-  public static final class DescriptorImpl extends Descriptor<SecurePostScript> {
-
-    private Result runCondition = Result.UNSTABLE;
-    private SecureGroovyScript secureGroovyScript;
-
-    public DescriptorImpl() {
-      load();
-    }
-
-    public SecureGroovyScript getSecureGroovyScript() {
-      return secureGroovyScript;
-    }
-
-    public String getDisplayName() {
-      return "Secure Post Script";
-    }
-
-    @Override
-    public boolean configure(final StaplerRequest req, final JSONObject formData) throws Descriptor.FormException {
-      final boolean sandbox = formData.getJSONObject("secureGroovyScript").getBoolean("sandbox");
-      final String rawScript = formData.getJSONObject("secureGroovyScript").getString("script");
-      final JSONArray paths = formData.getJSONObject("secureGroovyScript").optJSONArray("classpath");
-      final List<ClasspathEntry> cp = new ArrayList<ClasspathEntry>();
-      if (paths != null) {
-        for (int i = 0; i < paths.toArray().length; i++) {
-          final String path = JSONObject.fromObject(paths.get(i)).getString("path");
-          try {
-            cp.add(new ClasspathEntry(path));
-          } catch (final MalformedURLException e) {
-            e.printStackTrace();
-          }
-        }
-      }
-
-      this.secureGroovyScript = new SecureGroovyScript(rawScript, sandbox, cp)
-        .configuring(ApprovalContext.create().withCurrentUser());
-      runCondition = Result.fromString(formData.getString("runCondition"));
-      save();
-      return super.configure(req, formData);
-    }
-
-    public Result getResultCondition() {
-      return runCondition;
-    }
-
-    public String getRunCondition() {
-      return runCondition.toString();
     }
   }
 }
---
+++ src/main/java/io/jenkins/plugins/securepostscript/SecurePostScriptConfiguration.java
@@ -0,0 +1,65 @@
+package io.jenkins.plugins.securepostscript;
+
+import hudson.Extension;
+import hudson.model.Result;
+import hudson.util.FormValidation;
+import jenkins.model.GlobalConfiguration;
+import net.sf.json.JSONObject;
+
+import org.jenkinsci.plugins.scriptsecurity.sandbox.groovy.SecureGroovyScript;
+import org.kohsuke.stapler.DataBoundSetter;
+import org.kohsuke.stapler.QueryParameter;
+import org.kohsuke.stapler.StaplerRequest;
+
+@Extension
+public class SecurePostScriptConfiguration extends GlobalConfiguration {
+
+  private SecureGroovyScript secureGroovyScript;
+  
+  private Result runCondition = Result.SUCCESS;
+
+  public static SecurePostScriptConfiguration get() {
+    return jenkins.model.GlobalConfiguration.all().get(SecurePostScriptConfiguration.class);
+  }
+
+  public SecurePostScriptConfiguration() {
+    load();
+  }
+
+  public SecureGroovyScript getSecureGroovyScript() {
+    return secureGroovyScript;
+  }
+
+  /**
+   * Together with {@link #getSecureGroovyScript}, binds to entry in
+   * {@code config.jelly}.
+   *
+   * @param secureGroovyScript the new value of this field
+   */
+  @DataBoundSetter
+  public void setSecureGroovyScript(SecureGroovyScript secureGroovyScript) {
+    System.out.print("setSecureGroovyScript is called with: " + secureGroovyScript);
+    this.secureGroovyScript = secureGroovyScript;
+    save();
+  }
+  
+  public Result getResultCondition() {
+    return runCondition;
+  }
+
+  public String getRunCondition() {
+    return runCondition.toString();
+  }
+
+  @Override
+  public boolean configure(StaplerRequest req, JSONObject json) throws FormException {
+    this.runCondition = Result.fromString(json.getString("runCondition"));
+    save();
+    return super.configure(req, json);
+  }
+
+  public FormValidation doCheckScriptContent(@QueryParameter String value) {
+    return FormValidation.ok();
+  }
+
+}
